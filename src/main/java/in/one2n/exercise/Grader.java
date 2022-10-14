e4f544e338d189cd82441a30878a2b33d2f7dc45
ankit bora <ankitbora31@gmail.com>
2022-10-14 22:28:47 +0530

    completed

--- src/main/java/in/one2n/exercise/Grader.java
+++ src/main/java/in/one2n/exercise/Grader.java
@@ -1,29 +1,110 @@
 package in.one2n.exercise;
 
+import java.io.FileReader;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
 public class Grader {
 
-    public List<Student> parseCSV(String filepath) {
-        // TODO: add your implementation here
-        return Collections.emptyList();
-    }
+	public List<Student> parseCSV(String filepath) {
+		// TODO: add your implementation here
+		
+		// List of Student objects
+		List<Student> students = new ArrayList<Student>();
+		
+		// Store the CSV data in String List
+		List<String[]> allData;
+		
+		// Exception handler for the CSV path 
+		// collect all the data to allData list and
+		// convert this data to student object
+		// further these student objects are stored in students list
+		try {
+			FileReader filereader = new FileReader(filepath);
+			CSVReader csvReader = new CSVReaderBuilder(filereader).withSkipLines(1).build();
+			allData = csvReader.readAll();
 
-    public List<Student> calculateGrade(List<Student> students) {
-        // TODO: add your implementation here
-        return Collections.emptyList();
-    }
+			for (String[] row : allData) {
+				Student student = new Student(row[0], row[1], row[2],
+						Double.parseDouble(row[3]),Double.parseDouble(row[4]),
+						Double.parseDouble(row[5]),Double.parseDouble(row[6]));
+				students.add(student);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-    public Student findOverallTopper(List<Student> gradedStudents) {
-        // TODO: add your implementation here
-        return null;
-    }
+		return students;
+	}
 
-    public Map<String, Student> findTopperPerUniversity(List<Student> gradedStudents) {
-        // TODO: add your implementation here
-        return new HashMap<>();
-    }
+	public List<Student> calculateGrade(List<Student> students) {
+		// TODO: add your implementation here
+		
+		// generating a new list for students with final score
+		List<Student> gradedStudents = List.copyOf(students);
+		
+		for (Student st : gradedStudents) {
+			Double finalScore = st.getFinalScore();
+			st.setFinalScore(finalScore);
+		}
+		return gradedStudents;
+	}
+
+	public Student findOverallTopper(List<Student> gradedStudents) {
+		// TODO: add your implementation here
+		
+		Student student = null;
+		Double score = Double.MIN_VALUE;
+		for (Student st: gradedStudents) {
+			if (st.getFinalScore() > score) {
+				score = st.getFinalScore();
+				student = st;
+			}
+		}
+		return student;
+	}
+
+//	public Map<String, Student> findTopperPerUniversity(List<Student> gradedStudents) {
+//		// TODO: add your implementation here
+//
+//		Map<String, Student> univ = new HashMap<>();
+//
+//		for (int i = 0; i < gradedStudents.size(); i++) {
+//			String key = gradedStudents.get(i).getUniversity();
+//			if (univ.containsKey(key)) {
+//				Double score = univ.get(key).getFinalScore();
+//				for (int j = i; j < gradedStudents.size(); j++) {
+//					if (key == gradedStudents.get(j).getUniversity()) {
+//						Student st = gradedStudents.get(j);
+//						if (score < st.getFinalScore()) {
+//							univ.put(key, st);
+//							score = st.getFinalScore();
+//						}
+//					}
+//				}
+//			} else {
+//				univ.put(key, gradedStudents.get(i));
+//			}
+//		}
+//
+//		return univ;
+//	}
+	
+	public Map<String, Student> findTopperPerUniversity(List<Student> gradedStudents) {
+		// TODO: add your implementation here
+		// using streams
+		Map<String, Student> result = gradedStudents.stream()
+	        .collect(Collectors.groupingBy(Student::getUniversity, Collectors.collectingAndThen(Collectors
+	        		.maxBy(Comparator.comparing(Student::getFinalScore)), r -> r.get())));
+		return result;
+	}
 }
--- src/main/java/in/one2n/exercise/Student.java
+++ src/main/java/in/one2n/exercise/Student.java
@@ -1,5 +1,9 @@
 package in.one2n.exercise;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 public class Student {
 
     private String firstname;
@@ -10,27 +14,81 @@ public class Student {
     private Double test3Score;
     private Double test4Score;
 
-    // computed fields
+
+	// computed fields
     private Double finalScore;
     private Grade grade;
+    
+    public String getUniversity() {
+    	return this.university;
+    }
 
-    public Student(String firstname, String lastname, String university) {
+	public Student(String firstname, String lastname, String university) {
         // TODO: add your implementation here
+    	this.firstname = firstname;
+    	this.lastname = lastname;
+    	this.university = university;
     }
 
     public Student(String firstname, String lastname, String university, Double test1Score, Double test2Score, Double test3Score, Double test4Score) {
         // TODO: add your implementation here
+    	this.firstname = firstname;
+    	this.lastname = lastname;
+    	this.university = university;
+    	this.test1Score = test1Score;
+    	this.test2Score = test2Score;
+    	this.test3Score = test3Score;
+    	this.test4Score = test4Score;
     }
 
     public Double getFinalScore() {
         // TODO: add your implementation here
-        return null;
+    	Double Score = test1Score + test2Score + test3Score + test4Score;
+    	Score = Score / 4;
+        return Score;
     }
+    
+    public void setFinalScore(Double finalScore) {
+    	this.finalScore = finalScore;
+    }
+    
 
-    public Grade getGrade() {
+	public Grade getGrade() {
         // TODO: add your implementation here
-        return null;
+    	if (finalScore >= 70) {
+    		return Grade.A;
+    	}else if (finalScore >= 50) {
+    		return Grade.B;
+    	}else if(finalScore >= 35) {
+    		return Grade.C;
+    	}
+    	return Grade.F;
     }
+	
+	public void setGrade(Grade grade) {
+		this.grade = grade;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Student student = (Student) o;
+		return new EqualsBuilder()
+				.append(firstname,student.firstname)
+				.append(lastname,student.lastname)
+				.append(university,student.university)
+				.isEquals();
+	}
+	
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(17,37)
+				.append(firstname)
+				.append(lastname)
+				.append(university)
+				.toHashCode();
+	}
 
 }
 
